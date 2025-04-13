2e9cc2615a7a157662613eba7095330d9951d4e5
dickyjoelsaputra <dickyjoelsaputra@gmail.com>
2025-04-13 17:07:21 +0700

    s

--- core/custom_storages.py
+++ core/custom_storages.py
@@ -19,3 +19,14 @@ class MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
+
+    def url(self, name, parameters=None, expire=None):
+        url = super().url(name, parameters, expire)
+        # Ensure we're using the correct domain and bucket path
+        custom_domain = self.connection.meta.client.meta.endpoint_url
+        if not custom_domain.startswith('https://'):
+            custom_domain = f'https://{custom_domain}'
+        return url.replace(
+            f"{custom_domain}/",
+            f"{custom_domain}/{self.bucket_name}/"
+        )
